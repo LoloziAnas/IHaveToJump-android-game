a2aaa6ec6eb2ad15ff42db7a8c5cae07fc980016
LoloziAnas <lolozianas1@gmail.com>
2021-01-07 19:22:38 +0100

    creating gameView class

--- app/src/main/java/com/lzi/ihavetojump/MainActivity.java
+++ app/src/main/java/com/lzi/ihavetojump/MainActivity.java
@@ -2,7 +2,11 @@ package com.lzi.ihavetojump;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setting the orientation Landscape
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //binding the play button
+        TextView textViewPlay = findViewById(R.id.btn_play);
+        textViewPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,GameActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
